2f09d3d67eabdd57ca020fc912e3bab787ed090f
Stephen Clark <clark7382@hotmail.com>
2021-03-29 11:35:21 -0600

    reworking files

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -12,7 +12,7 @@ export class Login extends React.Component {
         <div className="header">Login</div>
         <div className="content">
           <div className="image">
-            <img src={loginImg} />
+            <img src="https://images.unsplash.com/photo-1614330258898-86823c7e3de2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y29tcHV0ZXIlMjB3b3JrfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
           </div>
           <div className="form">
             <div className="form-group">
