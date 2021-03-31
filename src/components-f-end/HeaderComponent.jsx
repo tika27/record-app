1ae69692b115cb742fd576713f2f365339726d06
Stephen Clark <clark7382@hotmail.com>
2021-03-30 18:34:36 -0600

    making changes to code

--- src/components-f-end/HeaderComponent.jsx
+++ src/components-f-end/HeaderComponent.jsx
@@ -14,6 +14,7 @@ class HeaderComponent extends Component {
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="https://javaguides.net" className="navbar-brand">Employee Information System</a></div>
                     </nav>
                 </header>
             </div>
