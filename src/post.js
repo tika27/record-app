968023b09125d04941b7cdbe6e84777b3083bdcb
Neamen_Dobamo <Neamenbirhanu@gmail.com>
2021-04-02 14:17:27 -0600

    fix file

--- src/post.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-var regSchema = new mongoose.Schema({
-    firstName: String,
-    lastNameName: String
-});
-
-var User = mongoose.model("User", regSchema);
