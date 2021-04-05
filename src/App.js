c4acf1dcac159c5cf5a92d296061bb7f2954387f
Stephen Clark <clark7382@hotmail.com>
2021-04-04 19:25:40 -0600

    making changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
-<<<<<<< HEAD
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
-import UpdateEmployeeComponent from "./components/UpdateEmployeeComponent";
 import ViewEmployeeComponent from "./components/ViewEmployeeComponent";
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
-import CreateEmployeeComponent from './components/CreateEmployeeComponent';
-import ViewEmployeeComponent from './components/ViewEmployeeComponent';
-import DashboardComponent from './components/DashboardComponent';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
-import CreeateSalaryComponent from './components/SalaryComponents/CreateSalaryComponent';
-import ListSalaryComponent from './components/SalaryComponents/ListSalaryComponent';
-import ViewSalaryComponent from './components/SalaryComponents/ViewSalaryComponent';
-
->>>>>>> origin
+import DashboardComponent from "./components/DashboardComponent";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import CreeateSalaryComponent from "./components/SalaryComponents/CreateSalaryComponent";
+import ListSalaryComponent from "./components/SalaryComponents/ListSalaryComponent";
+import ViewSalaryComponent from "./components/SalaryComponents/ViewSalaryComponent";
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <Router>
         <HeaderComponent />
         <div className="container">
@@ -44,33 +31,22 @@ function App() {
               path="/view-employee/:id"
               component={ViewEmployeeComponent}
             ></Route>
+            <Route path="/dashboard" component={DashboardComponent}></Route>
+            <Route
+              path="/add-salary/:id"
+              component={CreeateSalaryComponent}
+            ></Route>
+            <Route path="/salaries" component={ListSalaryComponent}></Route>
+            <Route
+              path="/view-salary/:id"
+              component={ViewSalaryComponent}
+            ></Route>
+
             {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
           </Switch>
         </div>
         <FooterComponent />
       </Router>
-=======
-        <Router>
-              <HeaderComponent />
-                <div className="container">
-                    <Switch> 
-                          <Route path = "/" exact component = {ListEmployeeComponent}></Route>
-                          <Route path = "/employees" component = {ListEmployeeComponent}></Route>
-                          <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
-                          <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
-                          <Route path = "/dashboard" component = {DashboardComponent}></Route>
-                          <Route path = "/add-salary/:id" component = {CreeateSalaryComponent}></Route>
-                          <Route path = "/salaries" component = {ListSalaryComponent}></Route>
-                          <Route path = "/view-salary/:id" component = {ViewSalaryComponent}></Route>
-
-
-
-                          {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
-                    </Switch>
-                </div>
-              <FooterComponent />
-        </Router>
->>>>>>> origin
     </div>
   );
 }
